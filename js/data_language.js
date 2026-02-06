f364f97306845ba7bbc03dc1cac913aba3cb2a3d
Lie Colirei <liecolirei@gmail.com>
2026-02-06 23:12:59 +0100

    First pass on tools and languages.

---
+++ js/data_language.js
@@ -0,0 +1,329 @@
+data_language = [
+    // --- LANGUES STANDARD ---
+    {
+        title: "Commun",
+        icon: "speech-bubble",
+        subtitle: "Langue commerciale et humaine",
+        description: "La langue la plus répandue des Royaumes",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : La plupart des créatures humanoïdes.",
+            "Alphabet : Commun",
+            "C'est la langue typique du commerce et de la diplomatie."
+        ]
+    },
+    {
+        title: "Nain",
+        icon: "mining-helmet", // ou "hammer-drop"
+        subtitle: "Langue du peuple Nain",
+        description: "Une langue aux consonnes dures et sons gutturaux",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Nains.",
+            "Alphabet : Nain",
+            "Sert aussi de base pour les langues Terran, Gnomique, Gobelin, Géant et Orc."
+        ]
+    },
+    {
+        title: "Elfique",
+        icon: "leaf-swirl",
+        subtitle: "Langue fluide et mélodieuse",
+        description: "Une langue aux intonations subtiles et grammaire complexe",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Elfes.",
+            "Alphabet : Elfique",
+            "Sert aussi de base pour le Sylvain et l'Outre-Commun."
+        ]
+    },
+    {
+        title: "Géant",
+        icon: "giant",
+        subtitle: "Langue des Ordning",
+        description: "Une version ancienne et simplifiée du Nain",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Ogres, Géants.",
+            "Alphabet : Nain"
+        ]
+    },
+    {
+        title: "Gnomique",
+        icon: "gears",
+        subtitle: "Langue des Gnomes",
+        description: "Utilise l'alphabet nain",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Gnomes.",
+            "Alphabet : Nain",
+            "Possède une structure technique et précise."
+        ]
+    },
+    {
+        title: "Gobelin",
+        icon: "goblin-head",
+        subtitle: "Langue des gobelinoïdes",
+        description: "Utilise l'alphabet nain",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Gobelins, Hobgobelins, Bugbears.",
+            "Alphabet : Nain"
+        ]
+    },
+    {
+        title: "Halfelin",
+        icon: "beer-stein",
+        subtitle: "Langue des petites gens",
+        description: "Souvent une tradition orale",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Halfelins.",
+            "Alphabet : Commun",
+            "N'est pas une langue secrète, mais les Halfelins n'aiment pas l'enseigner aux autres."
+        ]
+    },
+    {
+        title: "Orc",
+        icon: "orc-head",
+        subtitle: "Langue dure et rude",
+        description: "Utilise l'alphabet nain",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Orcs.",
+            "Alphabet : Nain",
+            "Manque de vocabulaire pour les concepts subtils."
+        ]
+    },
+
+    // --- LANGUES EXOTIQUES ---
+    {
+        title: "Abyssal",
+        icon: "imp-laugh",
+        subtitle: "Langue des Démons",
+        description: "Les sons chaotiques des abysses",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Démons, Gnolls.",
+            "Alphabet : Infernal",
+            "Une version corrompue et hurlante de l'Infernal."
+        ]
+    },
+    {
+        title: "Céleste",
+        icon: "angel-wings",
+        subtitle: "Langue des célestes",
+        description: "Transcendant et harmonieux",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Anges, Célestes.",
+            "Alphabet : Céleste"
+        ]
+    },
+    {
+        title: "Draconique",
+        icon: "dragon-head",
+        subtitle: "Langue des Dragons",
+        description: "Une langue ancienne et magique",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Dragons, Enfant-dragons, Kobolds.",
+            "Alphabet : Draconique",
+            "Souvent utilisée pour l'écriture de la magie arcanique."
+        ]
+    },
+    {
+        title: "Profond",
+        icon: "tentacles-skull",
+        subtitle: "Deep Speech",
+        description: "Langue des Flagelleurs Mentaux",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Aberrations, Flagelleurs Mentaux, Beholders.",
+            "Alphabet : Aucun (Télépathique/Mental)",
+            "Le son de cette langue est douloureux pour les non-initiés."
+        ]
+    },
+    {
+        title: "Infernal",
+        icon: "diablo-skull",
+        subtitle: "Langue des Diables",
+        description: "Structuré et manipulateur",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Diables, Tieffelins.",
+            "Alphabet : Infernal",
+            "Idéal pour rédiger des contrats inviolables."
+        ]
+    },
+    {
+        title: "Essenciel",
+        icon: "atomic-slashes",
+        subtitle: "Primordial",
+        description: "Langue des élémentaires",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Élémentaires.",
+            "Alphabet : Nain",
+            "Comprend les dialectes Aquatique, Aviaire, Pyrique et Terreux."
+        ]
+    },
+
+    {
+        title: "Aquatique",
+        icon: "wave-crest",
+        subtitle: "Dialecte de l'Essenciel",
+        description: "Langue de l'eau",
+        reference: "PHB, pg. 123.",
+        bullets: ["Dialecte parlé par les créatures marines et élémentaires d'eau."]
+    },
+    {
+        title: "Aviaire",
+        icon: "feather",
+        subtitle: "Dialecte de l'Essenciel",
+        description: "Langue de l'air",
+        reference: "PHB, pg. 123.",
+        bullets: ["Dialecte parlé par les élémentaires d'air et créatures volantes."]
+    },
+    {
+        title: "Pyrique",
+        icon: "flame",
+        subtitle: "Dialecte de l'Essenciel",
+        description: "Langue du feu",
+        reference: "PHB, pg. 123.",
+        bullets: ["Dialecte parlé par les élémentaires de feu, Ifrits et Azers."]
+    },
+    {
+        title: "Terreux",
+        icon: "rock",
+        subtitle: "Dialecte de l'Essenciel",
+        description: "Langue de la terre",
+        reference: "PHB, pg. 123.",
+        bullets: ["Dialecte parlé par les élémentaires de terre, Dao et Xorns."]
+    },
+
+
+    {
+        title: "Sylvain",
+        icon: "fairy",
+        subtitle: "Langue des fées",
+        description: "Utilise l'alphabet elfique",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Créatures fées, Centaures, Dryades.",
+            "Alphabet : Elfique",
+            "Une version plus ancienne et plus magique de l'Elfique."
+        ]
+    },
+    {
+        title: "Outre-Commun",
+        icon: "spider-web",
+        subtitle: "Undercommon",
+        description: "Langue commerciale de l'Outreterre",
+        reference: "PHB, pg. 123.",
+        bullets: [
+            "Parlé par : Drow, Duergar, Svirfneblin.",
+            "Alphabet : Elfique",
+            "Un mélange d'Elfique, de Nain et de Profond."
+        ]
+    },
+
+
+    // --- MISC / EXOTIQUES RARES (Tes ajouts) ---
+    {
+        title: "Vampire",
+        icon: "fangs",
+        subtitle: "Langue des non-morts nobles",
+        description: "Rarement entendue par les vivants",
+        reference: "MM / CoS",
+        bullets: ["Souvent associé aux dialectes anciens régionaux ou à l'Infernal."]
+    },
+    {
+        title: "Jypègue",
+        icon: "hippopotamus", // Si indispo: "musket"
+        subtitle: "Langue des Giff",
+        description: "Langue des mercenaires de l'espace",
+        reference: "Mordenkainen",
+        bullets: ["Souvent juste du Commun avec des termes militaires explosifs."]
+    },
+    {
+        title: "Gith",
+        icon: "psychic-waves",
+        subtitle: "Langue des Githyanki/Githzerai",
+        description: "Alphabet Tira'a",
+        reference: "Mordenkainen",
+        bullets: ["Langue gutturale issue de leur esclavage par les Flagelleurs."]
+    },
+    {
+        title: "Grung",
+        icon: "frog",
+        subtitle: "Langue des hommes-grenouilles",
+        description: "Croassements et cliquetis",
+        reference: "Volo",
+        bullets: ["Difficile à prononcer sans la physionomie d'un batracien."]
+    },
+    {
+        title: "Rodentien",
+        icon: "rat",
+        subtitle: "Langue des rats",
+        description: "Couinements compréhensibles par les wererats",
+        reference: "Homebrew / Lore",
+        bullets: ["Utilisé par les rats-garous et certaines créatures des égouts."]
+    },
+    {
+        title: "Runique",
+        icon: "runes",
+        subtitle: "Écriture de pouvoir",
+        description: "Souvent synonyme de l'alphabet Nain",
+        reference: "--",
+        bullets: ["Désigne souvent l'utilisation magique des runes plutôt qu'une langue parlée."]
+    },
+    {
+        title: "Kender",
+        icon: "pouch",
+        subtitle: "Langue des Kenders (Dragonlance)",
+        description: "Rapide et curieux",
+        reference: "Dragonlance",
+        bullets: ["Mélange de Commun et de termes argotiques propres aux Kenders."]
+    },
+    {
+        title: "Astral",
+        icon: "stars-stack",
+        subtitle: "Langue des plans",
+        description: "Langue des voyageurs astraux",
+        reference: "DMG",
+        bullets: ["Langue intemporelle utilisée par les habitants du plan Astral."]
+    },
+    {
+        title: "Thri-Keen",
+        icon: "mantis",
+        subtitle: "Langue insectoïde",
+        description: "Sans mots, que des cliquetis",
+        reference: "MM",
+        bullets: ["Impossible à parler pour un humanoïde sans mandibules. Utilise des claquements et des phéromones."]
+    },
+
+    {
+        title: "Druidique",
+        icon: "druid-staff", // ou "oak-leaf"
+        subtitle: "Langue secrète des druides",
+        description: "Interdit aux non-druides",
+        reference: "PHB, pg. 66.",
+        bullets: [
+            "Parlé par : Druides uniquement.",
+            "Alphabet : Druidique",
+            "Enseigner cette langue à un non-druide est passible de perte des pouvoirs."
+        ]
+    },
+    {
+        title: "Jobelin",
+        icon: "hood",
+        subtitle: "Thieves' Cant",
+        description: "Code secret des voleurs",
+        reference: "PHB, pg. 96.",
+        bullets: [
+            "Parlé par : Roublards.",
+            "Alphabet : Aucun (Codes et symboles)",
+            "Permet de cacher un message dans une conversation normale. Il faut 4 fois plus de temps pour transmettre l'idée."
+        ]
+    },
+]
---
+++ js/data_tools.js
@@ -0,0 +1,127 @@
+data_artisan = [
+    {
+        title: "Matériel d'Alchimiste",
+        icon: "standing-potion",
+        subtitle: "Concocter des potions",
+        description: "Deux béchers, un porte-bécher, un agitateur, un mortier et un pilon, une bourse d'ingrédients alchimiques",
+        bullets: [
+            "<b>Arcane</b> Vous obtenez plus d'informations grâce à vos jets d'Arcane concernant les potions ou autres matériaux similaires.",
+            "<b>Investigation</b> Lorsque vous fouillez une zone, vous récupérez plus d'informations sur les substances probablement utilisées dans la zone.",
+            "Vous pouvez fabriquer une dose d'acide, de feu grégeois, d'antitoxine, d'huile, de parfum ou de savon. (Moitié coût)",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Créer un nuage de fumée</td><td>10</td></tr><tr><td>Identifier un poison</td><td>10</td></tr><tr><td>Identifier une substance</td><td>15</td></tr><tr><td>Démarrer un feu</td><td>15</td></tr><tr><td>Neutraliser un acide</td><td>20</td></tr></table>"
+        ]
+    },
+    {
+        title: "Outils de Brasseur",
+        icon: "beer-stein",
+        subtitle: "Produire bière et eau pure",
+        description: "Une large carafe en verre, une quantité suffisante de houblon, un siphon et quelques mètres de tubes",
+        bullets: [
+            "<b>Histoire</b> Vous obtenez plus d'informations grâce à vos jets d'Histoire concernant les évènements où l'alcool a joué un rôle majeur.",
+            "<b>Médecine</b> Vous traitez plus facilement les symptômes de gueule de bois et vous utilisez plus habilement l'alcool comme anesthésiant.",
+            "<b>Persuasion</b> Vous pouvez plus facilement adoucir l'humeur d'une autre créature en l'inondant d'alcool.",
+            "<b>Eau Potable</b> Pendant un repos long, vous pouvez purifier jusqu'à 20 L d'eau, ou 3 L pendant un repos court.",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Détecter du poison ou des impuretés dans une boisson</td><td>10</td></tr><tr><td>Identifier un alcool</td><td>15</td></tr><tr><td>Ignorer les effets de l'alcool</td><td>20</td></tr></table>"
+        ]
+    },
+    {
+        title: "Matériel de Calligraphie",
+        icon: "ink-swirl",
+        subtitle: "Produire de belles écritures et pratiquer la graphologie",
+        description: "De l'encre, une douzaine de feuilles de parchemin et trois plumes",
+        bullets: [
+            "<b>Arcane</b> Vous identifiez plus facilement l'auteur d'un script magique.",
+            "<b>Histoire</b> Vous obtenez plus d'informations de vos jets visant à identifier d'anciennes écritures.",
+            "<b>Déchiffrer une Carte</b> Vous pouvez réaliser un jet d'Intelligence pour déterminer l'âge d'une carte, si elle contient des messages cachés...",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Identifier l'auteur d'un script non magique</td><td>10</td></tr><tr><td>Déterminer l'état d'esprit de l'auteur</td><td>15</td></tr><tr><td>Identifier un faux</td><td>15</td></tr><tr><td>Imiter une signature</td><td>20</td></tr></table>"
+        ]
+    },
+    {
+        title: "Outils de Charpentier",
+        icon: "hand-saw",
+        subtitle: "Construire des meubles et structures en bois",
+        description: "Une scie, un marteau, des clous, une hachette, une équerre, une règle, une herminette, un niveau et un ciseau",
+        bullets: [
+            "<b>Histoire</b> Vous identifiez plus facilement l'utilité et l'origine des structures en bois.",
+            "<b>Investigation</b> Vous obtenez des informations supplémentaires lorsque vous inspectez des structures en bois, puisque vous connaissez les trucs et astuces permettant de dissimuler certaines cachettes.",
+            "<b>Perception</b> Vous détectez plus facilement les pièges et les passages secrets en identifiant les irrégularités dans le bois.",
+            "<b>Discrétion</b> Vous pouvez facilement éviter les parties fragiles d'un sol en bois, qui pourraient grincer.",
+            "<b>Fortifier</b> En 1 minute, vous pouvez renforcer une fenêtre ou une porte. (DD +5)",
+            "<b>Abri Temporaire</b> Vous pouvez bâtir un abri pour vous garder au sec et au chaud pendant votre repos. Il s'effondre au bout de 1d3 jours.",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Construire une structure en bois simple</td><td>10</td></tr><tr><td>Concevoir une complexe structure en bois</td><td>15</td></tr><tr><td>Identifier le point faible d'un mur en bois</td><td>15</td></tr><tr><td>Dégonder une porte</td><td>20</td></tr></table>"
+        ]
+    },
+    {
+        title: "Outils de Cartographe",
+        icon: "sextant",
+        subtitle: "Dessiner des cartes de petite ou grande ampleur",
+        description: "Une plume, de l'encre, des parchemins, une paire de boussoles, un vernier et une règle",
+        bullets: [
+            "<b>Arcane, Histoire, Religion</b> Vous pouvez décerner plus d'informations grâce aux cartes, comme des messages cachés, des changements géologiques...",
+            "<b>Nature</b> Vous avez une meilleure connaissance du terrain.",
+            "<b>Survie</b> Vous avez une bonne intuition du fonctionnement des routes et de l'emplacement de villages.",
+            "<b>Dessiner une Carte</b> Vous pouvez dessiner une carte pendant vos voyages tout en conservant une autre activité.",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Déterminer l'âge d'une carte et son origine</td><td>10</td></tr><tr><td>Estimer la direction et la distance vers un repère</td><td>15</td></tr><tr><td>Identifier une fausse carte</td><td>15</td></tr><tr><td>Remplir les parties manquantes d'une carte</td><td>20</td></tr></table>"
+        ]
+    },
+    {
+        title: "Outils de Cordonnier",
+        icon: "leather-boot",
+        subtitle: "Fabriquer des paires de chaussures",
+        description: "Un marteau, un poinçon, un couteau, un support à chaussure, un cutter, du cuir et du fil",
+        bullets: [
+            "<b>Arcane, Histoire</b> Vous pouvez identifier les propriétés magiques de bottes enchantées ou leur histoire.",
+            "<b>Investigation</b> Vous pouvez déduire des informations des particules présentes sous les chaussures d'une créature et vous pouvez identifier l'origine de dommages sur des chaussures.",
+            "<b>Entretenir des Chaussures</b> Vous pouvez réparer les chaussures de vos compagnons, ce qui les rend capables de marcher sur une plus longue durée.",
+            "<b>Créer un Compartiment Caché</b> Vous pouvez ajouter un compartiment de 3x10 cm. Votre jet d'outil est le DD d'Investigation pour trouver le compartiment.",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Déterminer l'âge ou l'origine d'une chaussure</td><td>10</td></tr><tr><td>Trouver le compartiment caché dans le talon d'une botte</td><td>15</td></tr></table>"
+        ]
+    },
+    {
+        title: "Ustensiles de Cuisinier",
+        icon: "saucepan",
+        subtitle: "Cuisiner plats et friandises",
+        description: "Une casserole en métal, des couteaux, des fourchettes, une louche et une cuillère en bois",
+        bullets: [
+            "<b>Histoire</b> Vous identifiez les motifs sociaux qui dérivent des habitudes culinaires.",
+            "<b>Médecine</b> Vous pouvez adapter une mixture médicinale amère en un repas plaisant.",
+            "<b>Survie</b> Vous savez transformer des ingrédients que d'autres considéreraient inutilisables.",
+            "<b>Préparer des Repas</b> Vous pouvez préparer un repas pour aider vos alliés à se remettre. (+1 PV par DV pendant repos court)",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Cuisiner un repas simple</td><td>10</td></tr><tr><td>Reproduire un plat</td><td>15</td></tr><tr><td>Remarquer du poison ou des impuretés dans la nourriture</td><td>15</td></tr><tr><td>Cuisiner un plat gastronomique</td><td>15</td></tr></table>"
+        ]
+    },
+]
+
+
+data_misc = [
+
+    {
+        title: "Kit de Déguisement",
+        icon: "prank-glasses",
+        subtitle: "Se déguiser ou adopter une fausse identité",
+        description: "Du maquillage, des couleurs pour cheveux, quelques accessoires et vêtements",
+        bullets: [
+            "<b>Tromperie</b> Parfois, un déguisement peut aider à mentir de façon convaincante",
+            "<b>Intimidation</b> Le bon déguisement peut être effrayant, par exemple en tant que victime de la peste ou de membre d'un gang.",
+            "<b>Représentation</b> Un déguisement bien pensé peut exacerber la réaction de l'audience, en bien comme en mal.",
+            "<b>Persuasion</b> Les gens ont tendance à croire ceux en uniforme.",
+            "<b>Créer un Déguisement</b> Vous pouvez créer un déguisement complet en un repos long, un déguisement basé seulement sur l'apparence en 30 minutes et un déguisement plus modéré en 10 minutes.",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Dissimuler des blessures ou marques distinctives</td><td>10</td></tr><tr><td>Repérer le déguisement utilisé par un autre</td><td>15</td></tr><tr><td>Reproduire l'apparence d'un humanoïde</td><td>20</td></tr></table>"
+        ]
+    },
+    {
+        title: "Kit de Faussaire",
+        icon: "archive-register",
+        subtitle: "Reproduire des documents et imiter sceaux et signatures",
+        description: "Plusieurs encres, différentes variétés de parchemins et de papier, quelques plumes, des sceaux et de la cire, des feuilles d'or et d'argent ainsi que des petits outils pour tailler la cire avant d'imiter un sceau.",
+        bullets: [
+            "<b>Arcane</b> Vous pouvez utiliser vos connaissances pour déterminer si un objet magique est un faux ou non.",
+            "<b>Tromperie</b> De bons papiers donnent toujours plus de poids à un mensonge.",
+            "<b>Histoire</b> Vous pouvez évaluer l'authenticité d'un document historique.",
+            "<b>Investigation</b> Vous pouvez déterminer le mode de fabrication utilisé ainsi que l'authenticité d'un object.",
+            "<b>Autres Outils</b> Vous pouvez créer des faux en utilisant vos autres maîtrises d'outil",
+            "<b>Faux Vite-Fait</b> Faux d'une page au plus en un repos court. Faux de quatre pages en un repos long. Jet d'Intelligence comme DD pour indentifier la non-véracité.",
+            "<table><tr><th style='text-align:left'>Activité</th><th>DD</th></tr><tr><td>Imiter une écriture</td><td>15</td></tr><tr><td>Reproduire un sceau</td><td>20</td></tr></table>"
+        ]
+    },
+]
